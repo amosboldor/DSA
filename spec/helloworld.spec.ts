30c25a4245703124c766e134f55f1d3816fba7fc
Amos Boldor <amosboldor@protonmail.com>
2022-07-19 10:16:55 -0700

    learning D'oh!🍩

--- spec/helloworld.spec.ts
+++ spec/helloworld.spec.ts
@@ -1,7 +1,5 @@
 import { hello } from "../src/helloworld"
- 
-describe("Test Stuff", function() {
-  it("hello function returns world", function() {
-    expect(hello()).toBe("world");
-  });
+
+it("hello function returns world", function() {
+  expect(hello()).toBe("world");
 });
