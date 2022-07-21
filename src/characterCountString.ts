289fdcb8c052ee16586550db082dd44a772d40c8
Amos Boldor <amosboldor@protonmail.com>
2022-07-20 17:36:30 -0700

    adding comment explaining charCount function

--- src/characterCountString.ts
+++ src/characterCountString.ts
@@ -1,4 +1,5 @@
 export default function charCount(str: string): Map<string, number> {
+    // return a tally of every character in the given string
     let tally = new Map<string, number>();
     for (let i of str) {
         let chars: number | undefined = tally.get(i);
