b4cfaa13f2a60d5d6898bbb45d641b45f9402ef1
Amos Boldor <amosboldor@protonmail.com>
2022-07-22 18:23:37 -0700

    simplified validAnagram (takes up less space)

--- src/anagrams.ts
+++ src/anagrams.ts
@@ -1,28 +1,17 @@
 export default function validAnagram(str1: string, str2:string): boolean {
     // return true if str1 is an anagram of str2 (& vice versa) otherwiser return false
     if (str1 && str1.length === str2.length) {
-        const tally1 = new Map<string, number>();
-        const tally2 = new Map<string, number>();
+        const tally: {[key: string]: number} = {};
         for (let i = 0; i < str1.length; i++) {
             const charKey1 = str1.charAt(i).toLowerCase();
-            const charKey2 = str2.charAt(i).toLowerCase();
-            const chars1: number | undefined = tally1.get(charKey1);
-            const chars2: number | undefined = tally2.get(charKey2);
-            if (chars1) {
-                tally1.set(charKey1, chars1 + 1);
-            } else {
-                tally1.set(charKey1, 1);
-            }
-            if (chars2) {
-                tally2.set(charKey2, chars2 + 1);
-            } else {
-                tally2.set(charKey2, 1);
-            }
+            tally[charKey1] ? tally[charKey1] += 1 : tally[charKey1] = 1;
         }
-        for (let [key, value] of tally1) {
-            const tally2feq = tally2.get(key);
-            if (!tally2feq || tally2feq !== value) {
+        for (let i = 0; i < str2.length; i++) {
+            const charKey2 = str2.charAt(i).toLowerCase();
+            if (!tally[charKey2]) {
                 return false;
+            } else {
+                tally[charKey2] -= 1;
             }
         }
     } else {
