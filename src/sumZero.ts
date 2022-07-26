e03ffcc478c2a26ac128506b5a8398959dceffe5
Amos Boldor <amosboldor@protonmail.com>
2022-07-26 10:00:59 -0700

    cleaning up a bit and adding a comment explaining the function

--- src/sumZero.ts
+++ src/sumZero.ts
@@ -1,17 +1,20 @@
 export default function sumZero(integers: number[]): number[] | undefined {
+    // accepts a sorted array of integers
+    // the function should find the first pair where the sum is 0
+    // return an array that includes both values that sum to zero
+    // or undefined if a pair does not exist
     let leftPointer = 0;
     let rightPointer = integers.length - 1;
     while (leftPointer < rightPointer) {
         const leftInt = integers[leftPointer]!;
         const rightInt = integers[rightPointer]!;
         const leftRightSum = leftInt + rightInt;
-
         if (leftRightSum === 0) {
             return [leftInt, rightInt];
         } else if (leftRightSum > 0) {
-            rightPointer -= 1;
-        } else if (leftRightSum < 0) {
-            leftPointer += 1;
+            rightPointer--;
+        } else {
+            leftPointer++;
         }
     }
     return undefined;
