32fd08123692d0882654f6ad9e2e1970d96b071c
Amos Boldor <amosboldor@protonmail.com>
2022-07-28 18:03:38 -0700

    refactoring sameFrequency

--- src/sameFrequency.ts
+++ src/sameFrequency.ts
@@ -1,10 +1,11 @@
 export default function sameFrequency(num1: number, num2: number): boolean {
     // Given two positive intergers, find out if
     // the two numbers have the same frequency of digits.
-    let tally1 = new Map<string, number>();
-    let tally2 = new Map<string, number>();
-    const strNum1 = String(num1);
-    const strNum2 = String(num2);
+    const strNum1 = num1.toString();
+    const strNum2 = num2.toString();
+    if (strNum1.length !== strNum2.length) return false;
+    const tally1 = new Map<string, number>();
+    const tally2 = new Map<string, number>();
     for (let i = 0; i < strNum1.length; i++) {
         const digit1 = strNum1[i]!;
         const digits1: number | undefined = tally1.get(digit1);
