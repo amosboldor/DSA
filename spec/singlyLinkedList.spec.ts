b33ff3008c73b639ba4eea60ca9806212ce41e04
Amos Boldor <amosboldor@protonmail.com>
2022-08-02 13:18:40 -0700

    keeping tests DRY in SinglyLinkedList

--- spec/singlyLinkedList.spec.ts
+++ spec/singlyLinkedList.spec.ts
@@ -17,12 +17,22 @@ describe("Node", function(){
 });
 
 describe("SinglyLinkedList", function(){
+    let sll: SinglyLinkedList;
+    function sllInsert(n: number, location: "h" | "t") {
+        for (let data = 1; data <= n; data++) {
+            if (location === "h") {
+                sll.insertHead(data);
+            } else {
+                sll.insertTail(data);
+            }
+        }
+    }
+    beforeEach(function() {
+        sll = new SinglyLinkedList();
+    });
     it("size increments as expected", function(){
-        const sll = new SinglyLinkedList();
         expect(sll.size).toBe(0);
-        sll.insertHead(1);
-        sll.insertHead(2);
-        sll.insertHead(3);
+        sllInsert(3, "h");
         expect(sll.size).toBe(3);
     });
     it("head & tail are null when empty", function(){
@@ -30,33 +40,21 @@ describe("SinglyLinkedList", function(){
         expect(new SinglyLinkedList().tail).toBeNull();
     });
     it("insertHead adds node at the head", function(){
-        const sll = new SinglyLinkedList();
-        sll.insertHead(1);
-        sll.insertHead(2);
-        sll.insertHead(3);
-        sll.insertHead(4);
+        sllInsert(4, "h");
         expect(sll.head!.data).toBe(4);
         expect(sll.head!.next!.data).toBe(3);
         expect(sll.head!.next!.next!.data).toBe(2);
         expect(sll.tail!.data).toBe(1);
     });
     it("insertTail adds node at the tail", function(){
-        const sll = new SinglyLinkedList();
-        sll.insertTail(1);
-        sll.insertTail(2);
-        sll.insertTail(3);
-        sll.insertTail(4);
+        sllInsert(4, "t");
         expect(sll.head!.data).toBe(1);
         expect(sll.head!.next!.data).toBe(2);
         expect(sll.head!.next!.next!.data).toBe(3);
         expect(sll.tail!.data).toBe(4);
     });
     it("is iterable", function(){
-        const sll = new SinglyLinkedList();
-        sll.insertTail(1);
-        sll.insertTail(2);
-        sll.insertTail(3);
-        sll.insertTail(4);
+        sllInsert(4, "t");
         let n = 1;
         for (let data of sll) {
             expect(data).toBe(n);
@@ -64,10 +62,8 @@ describe("SinglyLinkedList", function(){
         }
     });
     it("clear method clears the list", function(){
-        const sll = new SinglyLinkedList();
-        sll.insertTail(1);
-        sll.insertHead(2);
-        sll.insertHead(3);
+        sllInsert(3, "h");
+        sllInsert(4, "t");
         sll.clear()
         expect(sll.head).toBeNull();
         expect(sll.tail).toBeNull();
@@ -75,7 +71,6 @@ describe("SinglyLinkedList", function(){
     });
     describe("delete method finds/deletes first node matching data and returns deleted(bool)", function(){
         it("deletes node when only one exists", function(){
-            const sll = new SinglyLinkedList();
             sll.insertHead(1);
             expect(sll.delete(1)).toBeTrue();
             expect(sll.head).toBeNull();
@@ -83,9 +78,7 @@ describe("SinglyLinkedList", function(){
             expect(sll.size).toBe(0);
         });
         it("deletes node(head) when only two node exists", function(){
-            const sll = new SinglyLinkedList();
-            sll.insertHead(1);
-            sll.insertHead(2);
+            sllInsert(2, "h");
             expect(sll.delete(2)).toBeTrue();
             expect(sll.head).toBeTruthy();
             expect(sll.tail).toBeTruthy();
@@ -94,9 +87,7 @@ describe("SinglyLinkedList", function(){
             expect(sll.size).toBe(1);
         });
         it("deletes node(tail) when only two node exists", function(){
-            const sll = new SinglyLinkedList();
-            sll.insertHead(1);
-            sll.insertHead(2);
+            sllInsert(2, "h");
             expect(sll.delete(1)).toBeTrue();
             expect(sll.head).toBeTruthy();
             expect(sll.tail).toBeTruthy();
@@ -105,40 +96,23 @@ describe("SinglyLinkedList", function(){
             expect(sll.size).toBe(1);
         });
         it("returns false and doesn't delete if data asked to delete doesn't exist", function(){
-            const sll = new SinglyLinkedList();
-            sll.insertHead(1);
-            sll.insertHead(2);
+            sllInsert(2, "h");
             expect(sll.size).toBe(2);
             expect(sll.delete(6)).toBeFalse();
             expect(sll.size).toBe(2);
         });
         it("deletes node(head) when three or more nodes exist", function(){
-            const sll = new SinglyLinkedList();
-            sll.insertHead(1);
-            sll.insertHead(2);
-            sll.insertHead(3);
-            sll.insertHead(4);
-            sll.insertHead(5);
+            sllInsert(5, "h");
             expect(sll.delete(5)).toBeTrue();
             expect(sll.head!.data).toBe(4);
         });
         it("deletes node(middle) when three or more nodes exist", function(){
-            const sll = new SinglyLinkedList();
-            sll.insertHead(1);
-            sll.insertHead(2);
-            sll.insertHead(3);
-            sll.insertHead(4);
-            sll.insertHead(5);
+            sllInsert(5, "h");
             expect(sll.delete(1)).toBeTrue();
             expect(sll.tail!.data).toBe(2);
         });
         it("deletes node(tail) when three or more nodes exist", function(){
-            const sll = new SinglyLinkedList();
-            sll.insertHead(1);
-            sll.insertHead(2);
-            sll.insertHead(3);
-            sll.insertHead(4);
-            sll.insertHead(5);
+            sllInsert(5, "h");
             expect(sll.delete(3)).toBeTrue();
             expect(sll.head!.next!.data).toBe(4);
             expect(sll.head!.next!.next!.data).toBe(2);
