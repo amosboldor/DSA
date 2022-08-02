c79c8b603ca8d69dcc4d17989c9281adcfe99297
Amos Boldor <amosboldor@protonmail.com>
2022-08-02 12:32:43 -0700

    tweaking insert(Head/Tail) methods, adding delete/clean method with corresponding tests

--- spec/singlyLinkedList.spec.ts
+++ spec/singlyLinkedList.spec.ts
@@ -1,13 +1,13 @@
 import { Node, SinglyLinkedList } from "../src/singlyLinkedList";
 
-describe("Node class functions as expected", function(){
-    it("Node has data property", function(){
+describe("Node", function(){
+    it("has data property", function(){
         expect(new Node(1).data).toBe(1);
     });
-    it("Node has null next (node) property", function(){
+    it("has null next (node) property", function(){
         expect(new Node(1).next).toBeNull();
     });
-    it("Node has correct Node in next property", function(){
+    it("has correct Node in next property", function(){
         const x = new Node(2);
         const y = new Node(1);
         y.next = x;
@@ -16,15 +16,20 @@ describe("Node class functions as expected", function(){
     });
 });
 
-describe("SinglyLinkedList IS", function(){
-    it("SinglyLinkedList size is 0", function(){
-        expect(new SinglyLinkedList().size).toBe(0);
+describe("SinglyLinkedList", function(){
+    it("size increments as expected", function(){
+        const sll = new SinglyLinkedList();
+        expect(sll.size).toBe(0);
+        sll.insertHead(1);
+        sll.insertHead(2);
+        sll.insertHead(3);
+        expect(sll.size).toBe(3);
     });
-    it("SinglyLinkedList head & tail are null", function(){
+    it("head & tail are null when empty", function(){
         expect(new SinglyLinkedList().head).toBeNull();
         expect(new SinglyLinkedList().tail).toBeNull();
     });
-    it("SinglyLinkedList has 4 node in order (insertHead)", function(){
+    it("insertHead adds node at the head", function(){
         const sll = new SinglyLinkedList();
         sll.insertHead(1);
         sll.insertHead(2);
@@ -35,7 +40,7 @@ describe("SinglyLinkedList IS", function(){
         expect(sll.head!.next!.next!.data).toBe(2);
         expect(sll.tail!.data).toBe(1);
     });
-    it("SinglyLinkedList has 4 node in order (insertTail)", function(){
+    it("insertTail adds node at the tail", function(){
         const sll = new SinglyLinkedList();
         sll.insertTail(1);
         sll.insertTail(2);
@@ -46,7 +51,7 @@ describe("SinglyLinkedList IS", function(){
         expect(sll.head!.next!.next!.data).toBe(3);
         expect(sll.tail!.data).toBe(4);
     });
-    it("SinglyLinkedList is iterable", function(){
+    it("is iterable", function(){
         const sll = new SinglyLinkedList();
         sll.insertTail(1);
         sll.insertTail(2);
@@ -57,5 +62,86 @@ describe("SinglyLinkedList IS", function(){
             expect(data).toBe(n);
             n++;
         }
+    });
+    it("clear method clears the list", function(){
+        const sll = new SinglyLinkedList();
+        sll.insertTail(1);
+        sll.insertHead(2);
+        sll.insertHead(3);
+        sll.clear()
+        expect(sll.head).toBeNull();
+        expect(sll.tail).toBeNull();
+        expect(sll.size).toBe(0);
+    });
+    describe("delete method finds/deletes first node matching data and returns deleted(bool)", function(){
+        it("deletes node when only one exists", function(){
+            const sll = new SinglyLinkedList();
+            sll.insertHead(1);
+            expect(sll.delete(1)).toBeTrue();
+            expect(sll.head).toBeNull();
+            expect(sll.tail).toBeNull();
+            expect(sll.size).toBe(0);
+        });
+        it("deletes node(head) when only two node exists", function(){
+            const sll = new SinglyLinkedList();
+            sll.insertHead(1);
+            sll.insertHead(2);
+            expect(sll.delete(2)).toBeTrue();
+            expect(sll.head).toBeTruthy();
+            expect(sll.tail).toBeTruthy();
+            expect(sll.head).toBe(sll.tail);
+            expect(sll.head!.data).toBe(1);
+            expect(sll.size).toBe(1);
+        });
+        it("deletes node(tail) when only two node exists", function(){
+            const sll = new SinglyLinkedList();
+            sll.insertHead(1);
+            sll.insertHead(2);
+            expect(sll.delete(1)).toBeTrue();
+            expect(sll.head).toBeTruthy();
+            expect(sll.tail).toBeTruthy();
+            expect(sll.head).toBe(sll.tail)
+            expect(sll.head!.data).toBe(2);
+            expect(sll.size).toBe(1);
+        });
+        it("returns false and doesn't delete if data asked to delete doesn't exist", function(){
+            const sll = new SinglyLinkedList();
+            sll.insertHead(1);
+            sll.insertHead(2);
+            expect(sll.size).toBe(2);
+            expect(sll.delete(6)).toBeFalse();
+            expect(sll.size).toBe(2);
+        });
+        it("deletes node(head) when three or more nodes exist", function(){
+            const sll = new SinglyLinkedList();
+            sll.insertHead(1);
+            sll.insertHead(2);
+            sll.insertHead(3);
+            sll.insertHead(4);
+            sll.insertHead(5);
+            expect(sll.delete(5)).toBeTrue();
+            expect(sll.head!.data).toBe(4);
+        });
+        it("deletes node(middle) when three or more nodes exist", function(){
+            const sll = new SinglyLinkedList();
+            sll.insertHead(1);
+            sll.insertHead(2);
+            sll.insertHead(3);
+            sll.insertHead(4);
+            sll.insertHead(5);
+            expect(sll.delete(1)).toBeTrue();
+            expect(sll.tail!.data).toBe(2);
+        });
+        it("deletes node(tail) when three or more nodes exist", function(){
+            const sll = new SinglyLinkedList();
+            sll.insertHead(1);
+            sll.insertHead(2);
+            sll.insertHead(3);
+            sll.insertHead(4);
+            sll.insertHead(5);
+            expect(sll.delete(3)).toBeTrue();
+            expect(sll.head!.next!.data).toBe(4);
+            expect(sll.head!.next!.next!.data).toBe(2);
+        });
     });
 });
--- src/singlyLinkedList.ts
+++ src/singlyLinkedList.ts
@@ -12,7 +12,7 @@ class SinglyLinkedList {
 
     insertHead(data: any) {
         const newNode = new Node(data);
-        if (!this.head) {
+        if (!this.size) {
             this.head = newNode;
             this.tail = newNode;
         } else  {
@@ -23,13 +23,56 @@ class SinglyLinkedList {
         this.size++;
     }
 
+    delete(data: any): boolean {
+        const existsContainsData = (node: Pointer) => node && node.data === data;
+        let deleted = false;
+        if (this.size > 2) {
+            if (this.head!.data === data) {
+                this.head = this.head!.next;
+                deleted = true;
+            }
+            let beforeNode: Pointer = this.head!;
+            let nextNode: Pointer = beforeNode.next;
+            while (nextNode) {
+                if (existsContainsData(nextNode)) {
+                    if (nextNode === this.tail) {
+                        beforeNode!.next = null;
+                        this.tail = beforeNode;
+                    } else {
+                        beforeNode!.next = nextNode!.next;
+                    }
+                    deleted = true;
+                }
+                beforeNode = nextNode;
+                nextNode = nextNode!.next;
+            }
+        } else if (this.size === 2) {
+            if (existsContainsData(this.head)) {
+                this.head = this.tail;
+                deleted = true;
+            } else if (existsContainsData(this.tail)) {
+                this.tail = this.head;
+                deleted = true;
+            }
+        } else if (this.size === 1 && existsContainsData(this.head)) {
+            this.head = this.tail = null;
+            deleted = true;
+        }
+        return deleted ? Boolean(this.size--) : false;
+    }
+
+    clear() {
+        this.size = 0;
+        this.head = this.tail = null;
+    }
+
     insertTail(data: any) {
         const newNode = new Node(data);
-        if (!this.tail) {
+        if (!this.size) {
             this.head = newNode;
             this.tail = newNode;
         } else {
-            this.tail.next = newNode;
+            this.tail!.next = newNode;
             this.tail = newNode;
         }
         this.size++;
